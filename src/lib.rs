715b3ee1bb84b690bff5abedb998ee15468fe40a
Praveen Saravanan <praveen747757@gmail.com>
2022-12-29 16:05:23 +0530

    Build: generate native bind loader

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,50 @@
 #[macro_use]
 extern crate napi_derive;
 
+use std::path::Path;
+
+use napi::{
+  bindgen_prelude::*,
+  threadsafe_function::{
+    ErrorStrategy, ThreadSafeCallContext, ThreadsafeFunction, ThreadsafeFunctionCallMode,
+  },
+  JsExternal, JsString, JsUndefined,
+};
+use notify::{recommended_watcher, Event, RecommendedWatcher, RecursiveMode, Watcher};
+
+#[napi(ts_args_type = "dir: string, callback: (err: null | Error, event: string) => void")]
+pub fn watch(env: Env, dir: JsString, callback: JsFunction) -> Result<JsExternal> {
+  let dir = dir.into_utf8()?;
+
+  let tsfn: ThreadsafeFunction<Event, ErrorStrategy::CalleeHandled> = callback
+    .create_threadsafe_function(0, |cx: ThreadSafeCallContext<Event>| {
+      Ok(vec![cx
+        .env
+        .create_string_from_std(serde_json::to_string(&cx.value)?)?])
+    })?;
+
+  let mut watcher = recommended_watcher(move |ev: notify::Result<Event>| {
+    tsfn.call(
+      ev.map_err(|e| Error::new(Status::GenericFailure, format!("{}", e))),
+      ThreadsafeFunctionCallMode::NonBlocking,
+    );
+  })
+  .map_err(|e| Error::new(Status::GenericFailure, format!("{}", e)))?;
+
+  watcher
+    .watch(Path::new(dir.as_str()?), RecursiveMode::Recursive)
+    .map_err(|e| Error::new(Status::GenericFailure, format!("{}", e)))?;
+
+  env.create_external(watcher, None)
+}
+
 #[napi]
-pub fn sum(a: i32, b: i32) -> i32 {
-  a + b
+pub fn unwatch(env: Env, ext: JsExternal, dir: JsString) -> Result<JsUndefined> {
+  let dir = dir.into_utf8()?;
+  let watcher = env.get_value_external::<RecommendedWatcher>(&ext)?;
+
+  watcher
+    .unwatch(Path::new(dir.as_str()?))
+    .map_err(|e| Error::new(Status::GenericFailure, format!("{}", e)))?;
+  env.get_undefined()
 }
